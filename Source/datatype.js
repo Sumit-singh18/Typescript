305af5075f72b57229ad1ba7548b471632e3c343
Sumit-singh18 <ss7262692@gmail.com>
2023-06-30 14:42:33 +0530

    _A

--- Source/datatype.js
+++ Source/datatype.js
@@ -11,3 +11,17 @@ age = 88;
 let dob = "44";
 let result = parseInt(dob);
 console.log(result);
+// Boolean 
+// in js if you not give any value it will give false
+// In ts if you not assign any value it will give undefined
+let isValid = true;
+console.log(isValid);
+// Array
+let empList = ['Sumit', 'Sumit2', 'Sumit3'];
+console.log(empList);
+let depNum = [1, 2, 33, 4, 4, 3];
+console.log(depNum);
+let results = depNum.filter((depNum) => depNum > 2);
+console.log(results);
+let sum = depNum.reduce((acc, num) => acc + num);
+console.log(sum);
--- Source/datatype.ts
+++ Source/datatype.ts
@@ -15,3 +15,31 @@ age=88;
 let dob ="44";
 let  result = parseInt(dob)
 console.log(result);
+
+// Boolean 
+// in js if you not give any value it will give false
+// In ts if you not assign any value it will give undefined
+let isValid :boolean =true;
+
+console.log(isValid)
+
+
+// Array
+
+let empList :string[] =  ['Sumit', 'Sumit2', 'Sumit3']
+
+console.log(empList)
+
+
+let depNum : Array<number>= [ 1,2,33,4,4,3]
+console.log(depNum)
+
+// filter
+let results = depNum.filter((depNum)=> depNum>2);
+console.log(results)
+
+// Reduce
+let sum = depNum.reduce((acc,num)=>acc+num);
+console.log(sum)
+
+
