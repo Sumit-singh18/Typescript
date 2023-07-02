6bf39b2838eb65b88157ac5ffddbca639045208d
Sumit-singh18 <ss7262692@gmail.com>
2023-07-02 16:43:31 +0530

    A

--- Source/datatype.js
+++ Source/datatype.js
@@ -21,7 +21,32 @@ let empList = ['Sumit', 'Sumit2', 'Sumit3'];
 console.log(empList);
 let depNum = [1, 2, 33, 4, 4, 3];
 console.log(depNum);
+// filter
 let results = depNum.filter((depNum) => depNum > 2);
 console.log(results);
+// Reduce
 let sum = depNum.reduce((acc, num) => acc + num);
 console.log(sum);
+// ENUM
+var CardinalDirections;
+(function (CardinalDirections) {
+    CardinalDirections[CardinalDirections["North"] = 0] = "North";
+    CardinalDirections[CardinalDirections["East"] = 1] = "East";
+    CardinalDirections[CardinalDirections["South"] = 2] = "South";
+    CardinalDirections[CardinalDirections["West"] = 3] = "West";
+})(CardinalDirections || (CardinalDirections = {}));
+;
+let currentDirection = CardinalDirections.North;
+// North is the first value so it logs '0'
+console.log(currentDirection);
+var StatusCodes;
+(function (StatusCodes) {
+    StatusCodes[StatusCodes["NotFound"] = 404] = "NotFound";
+    StatusCodes[StatusCodes["Success"] = 200] = "Success";
+    StatusCodes[StatusCodes["Accepted"] = 202] = "Accepted";
+    StatusCodes[StatusCodes["BadRequest"] = 400] = "BadRequest";
+})(StatusCodes || (StatusCodes = {}));
+// logs 404
+console.log(StatusCodes.NotFound);
+// logs 200
+console.log(StatusCodes.Success);
--- Source/datatype.ts
+++ Source/datatype.ts
@@ -43,3 +43,31 @@ let sum = depNum.reduce((acc,num)=>acc+num);
 console.log(sum)
 
 
+// ENUM
+enum CardinalDirections {
+    North,
+    East,
+    South,
+    West
+  };
+              
+  let currentDirection = CardinalDirections.North;
+  
+  // North is the first value so it logs '0'
+  console.log(currentDirection);
+
+
+  enum StatusCodes {
+    NotFound = 404,
+    Success = 200,
+    Accepted = 202,
+    BadRequest = 400
+  }
+  // logs 404
+  console.log(StatusCodes.NotFound);
+  // logs 200
+  console.log(StatusCodes.Success);
+
+
+
+  
